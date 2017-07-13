271912491887edbb5b3b1f7c141f2083bee53b32
Cindi Harris <cmr0319@gmail.com>
2017-07-13 16:17:18 -0400

    finished other questions.

--- main.js
+++ main.js
@@ -7,60 +7,106 @@ console.log(data);
 
 
 // 1: Show me how to calculate the average price of all items.
-function question1 () {
+function question1() {
   // Answer:
   let totalOfPrices = 0;
   let averagePrice = 0;
-  for (let i = 0; i < data.length; i++){
+  for (let i = 0; i < data.length; i++) {
     totalOfPrices += data[i].price;
-  } console.log (averagePrice = totalOfPrices/data.length);
+  }
+  console.log(averagePrice = totalOfPrices / data.length);
 }
 
 
 
 // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
-function question2 () {
+function question2() {
   // Answer:
-let items = [];
+  let items = [];
   for (let i = 0; i < data.length; i++) {
 
     if (data[i].price >= 14 &&
-        data[i].price <= 18) {
-        items.push(data[i].title);
-        }
-  } console.log(items);
+      data[i].price <= 18) {
+      items.push(data[i].title);
+    }
+  }
+  console.log(items);
 }
 
 
 
 // 3: Which item has a "GBP" currency code? Display it's name and price.
-function question3 () {
+function question3() {
   // Answer:
-let gbp;
+  let gbp;
 
   for (let i = 0; i < data.length; i++) {
     if (data[i].currency_code === "GBP") {
       gbp = data[i].title + " $" + data[i].price + ".00";
     }
-  } console.log(gbp);
+  }
+  console.log(gbp);
 }
 
 
 // 4: Display a list of all items who are made of wood.
-function question4 () {
+function question4() {
   // Answer:
-}
 
+let woodItems=[];
+
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].materials.length; j++){
+    if(data[i].materials[j] == "wood") {
+
+      woodItems.push(data[i].title);
+
+
+    }
+
+  }
+} console.log(woodItems);
+}
 
 // 5: Which items are made of eight or more materials?
 //    Display the name, number of items and the items it is made of.
-function question5 () {
+function question5() {
   // Answer:
-}
+  let itemsList = [];
+
+    for (let i = 0; i < data.length; i++) {
+        let items = [];
+      if(data[i].materials.length >= 8) {
+
+        items.push(data[i].title,data[i].materials.length, data[i].materials);
+        itemsList.push(items);
+      }
+
+    } console.log(itemsList);
+  }
+
+
+ //return an array containing the string, number of the items, and the names of
+  //the items
 
 
 // 6: How many items were made by their sellers?
 // Answer:
-function question6 () {
+function question6() {
   // Answer:
+
+let handMade = [];
+
+
+  for (let i = 0; i < data.length; i++) {
+
+
+
+    if (data[i].who_made === "i_did") {
+
+      handMade.push(data[i].title);
+
+    }
+  } console.log(handMade.length);
+
 }
